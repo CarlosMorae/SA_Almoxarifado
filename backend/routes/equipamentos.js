b7443c078e36b94186cb1c44516f501e7e44bbfa
CarlosMorae <carlos_e_morais@estudante.sc.senai.br>
2026-05-19 21:40:22 -0300

    q

---
+++ backend/routes/equipamentos.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const connection = require("../database/conection")
+
+router.get("/listarTodos", (req, res) => {
+    const sql = "SELECT * FROM equipamentos";
+
+    connection.query(sql, (error, result) => {
+        if (error) {
+            return res.status(500).json(error);
+        };
+
+        res.status(200).json(result);
+    });
+});
+
+router.post("/criar", (req, res) => {
+    const {nome, marca, patrimonio} = req.body;
+    const sql = "INSERT INTO equipamentos (nome, marca, patrimonio) VALUES (?, ?, ?)"
+
+    connection.query(sql, [nome, marca, patrimonio], (error) => {
+        if (error) {
+            return res.status(500).json(error);
+        };
+
+        res.status(200).json({mensagem: "Equipamento cadastrado com sucesso!"})
+    });
+});
+
+module.exports = router;
