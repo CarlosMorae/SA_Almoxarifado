0685bd87f36c3e5921ca7d44c2be0767400e9087
CarlosMorae <carlos_e_morais@estudante.sc.senai.br>
2026-05-21 20:50:35 -0300

    Update equipamentos.js

--- backend/routes/equipamentos.js
+++ backend/routes/equipamentos.js
@@ -28,15 +28,32 @@ router.post("/criar", (req, res) => {
     });
 });
 
-router.put("./editar/:id", (req, res) => {
+router.put("/editar/:id", (req, res) => {
     const { id } = req.params;
     const { nome, marca, patrimonio } = req.body;
+    const sql = "UPDATE equipamentos SET nome = ?, marca = ?, patrimonio = ? WHERE id = ?"
+
+    connection.query(sql, [nome, marca, patrimonio, id], (error) => {
+        if (error) {
+            return res.status(500).json(error);
+        };
+
+        res.status(200).json({ mensagem: "Equipamento atualizado com sucesso!" })
+    });
 });
 
 router.delete("/excluir/:id", (req, res) => {
     const { id } = req.params;
-});
+    const sql = "DELETE FROM equipamentos WHERE id = ?"
 
+    connection.query(sql, [id], (error) => {
+        if (error) {
+            return res.status(500).json(error);
+        };
+
+        res.status(200).json({ mensagem: "Equipamento deletado com sucesso!" })
+    });
+});
 
 
 module.exports = router;
